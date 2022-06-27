0e113c73cf6d9f040e3f193146759e36b1927326
nekonoko114 <yoshua0826@gmail.com>
2022-06-27 14:04:14 +0900

    見た目部分一時的に完成今後修正必要あり,メニューJS実装 topへ戻るボタン実装

---
+++ dist/assets/js/base.js
@@ -0,0 +1,29 @@
+"use strict";
+
+$(function () {
+  $('.header__btn').click(function () {
+    $('.header__btn').toggleClass('active');
+    $('.header__nav').toggleClass('active');
+    $('.header__bg').toggleClass('active');
+  });
+  $('a[href^="#"]').on('click', function () {
+    $('.header__btn').removeClass('active');
+    $('.header__nav').removeClass('active');
+    $('.header__bg').removeClass('active');
+  });
+  var topPage = $('.topPage');
+  topPage.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 80) {
+      topPage.fadeIn();
+    } else {
+      topPage.fadeOut();
+    }
+  });
+  topPage.click(function () {
+    $('body , html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+});
